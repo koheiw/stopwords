1ec62f214be1d5fc0a65b1e8f2e6600cbd481f64
Kohei Watanabe <watanabe.kohei@gmail.com>
2017-11-30 12:23:47 +0000

    The first working version

--- R/stopwords.R
+++ R/stopwords.R
@@ -1,47 +1,53 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-
 #' Collection of stopwords in multiple langauges
 #'
 #' This function returns stopwrods colated for Stopwords ISO library
 #' (\url{https://github.com/stopwords-iso/stopwords-iso}).
-#' @param langauge ISO 639-1 language code
+#' @param langauge specify language of stopwords by ISO 639-1 code
 #' @return a character cector containing stopwords
 #' @export
 #'
 #' @examples
+#' languages() # returns all available languages
 #' stopwords('en')
 #' stopwords('jp')
 stopwords <- function(langauge = 'en') {
-    if (!(langauge %in% names(stopwords::data_char_stopwords_iso)))
-        stop(paste0("\"", kind, "\" is not a iso code for stopwords."))
-    stopwords::data_char_stopwords_iso[[langauge]]
+    load('data/data_char_stopwords_iso.RData')
+    if (!langauge %in% names(data_char_stopwords_iso))
+        stop(paste0("\"", langauge, "\" is not in Stopwords ISO library."))
+    data_char_stopwords_iso[[langauge]]
+}
+
+#' Returns all the ISO 639-1 code available in the Stopwords ISO library
+#'
+#' See \url{https://en.wikipedia.org/wiki/ISO_639-1} for details of the language code.
+#' @export
+#' @examples
+#' languages()
+languages <- function() {
+    load('data/data_char_stopwords_iso.RData')
+    names(data_char_stopwords_iso)
 }
 
 #' Internal function to update the data file
 #' @keywords internal
+#' @examples
+#' stopwords:::update_stopwords()
 update_stopwords <- function() {
 
     url <- 'https://raw.githubusercontent.com/stopwords-iso/stopwords-iso/master/stopwords-iso.json'
     json <- paste0(readLines(url, warn = FALSE), collapse="")
-    data <- rjson::fromJSON(json)
-    cat('Downloaded stopwords for', length(data), 'langauges\n')
-    name <- 'data_char_stopwords_iso.RData'
-    save(data, file = name)
-    cat('Saved as ', name, '\n')
+    data_char_stopwords_iso <- rjson::fromJSON(json)
+    cat('Downloaded stopwords for', length(data), 'langauges.\n')
+    path <- 'data/data_char_stopwords_iso.RData'
+    save(data_char_stopwords_iso, file = path)
+    cat('Saved to', path, '.\n')
+
 }
 
+#' RData file created from Stopwords ISO library
+#'
+#' @name data_char_stopwords_iso
+#' @docType data
+#' @keywords data
+NULL
 
